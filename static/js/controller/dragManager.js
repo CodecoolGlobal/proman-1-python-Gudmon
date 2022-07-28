708b2f81a1549fb6488dabb2fab5a7bf932dd88b
oem <rnagy89@gmail.com>
2022-07-28 15:23:20 +0200

    Feat: Add dragging animations

--- static/js/controller/dragManager.js
+++ static/js/controller/dragManager.js
@@ -6,10 +6,25 @@ let dragManager = {
 
     dragStart: function () {
         draggableStuff = this;
-
+        let dropTarget = document.querySelectorAll(".board-column-content")
+        console.log(dropTarget)
+        dropTarget.forEach(e => {
+            e.style.backgroundColor = "green"
+            e.style.opacity = "0.4"
+        })
+        this.style.opacity = "0.5"
+        this.style.transform = "scale(0.7)"
+        this.style.animation = "heartBeat 0.3s"
     },
 
     dragEnd: function () {
+        let dropTarget = document.querySelectorAll(".board-column-content")
+        dropTarget.forEach(e => {
+            e.style.backgroundColor = "lightgrey"
+            e.style.opacity = "1"
+        })
+        this.style.opacity = '1'
+        this.style.transform = "scale(1)"
         draggableStuff = null;
 
     },
@@ -20,19 +35,17 @@ let dragManager = {
     },
 
     dragEnter: function () {
-        this.style.border = '1px dashed #ccc'
-
+        this.style.border = "3px dashed"
     },
 
     dragLeave: function () {
-        this.style.border = 'none';
+        this.style.border = "none"
         console.log("dragleave");
     },
 
     dragDrop: function () {
         this.style.border = 'none';
         this.appendChild(draggableStuff);
-
     },
 
 
