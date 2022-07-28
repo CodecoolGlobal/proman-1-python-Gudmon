971a7077bb1b103237cf7bb3fb3cee53c2a73cfc
oem <rnagy89@gmail.com>
2022-07-28 17:38:31 +0200

    Fix: Add check on dragDrop, can no longer drop cards into other boards.

---
+++ static/js/controller/modalManager.js
@@ -0,0 +1,15 @@
+import {dataHandler} from "../data/dataHandler.js";
+import {htmlFactory, htmlTemplates} from "../view/htmlFactory.js";
+import {domManager} from "../view/domManager.js";
+import {cardsManager} from "./cardsManager.js";
+import {dragManager} from "../controller/dragManager.js";
+
+
+let modalManager = {
+    addNewCard: function () {
+
+    }
+
+
+
+}
--- static/js/view/htmlFactory.js
+++ static/js/view/htmlFactory.js
@@ -71,34 +71,65 @@ function cardBuilder(card) {
 }
 
 function modalBuilder(modal) {
-    return `<div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-  aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header text-center">
-        <h4 class="modal-title w-100 font-weight-bold">Sign in</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <div class="md-form">
-          <input type="email" id="defaultForm-email" class="form-control validate">
-          <label data-error="wrong" data-success="right" for="defaultForm-email">Your email</label>
+    return `
+      <div class="modal" id="test-modal">
+        <div class="modal-content">
+            <span class="close-button">&times;</span>
+            <p> Test </p>
         </div>
-
-        <div class="md-form">
-          <input type="password" id="defaultForm-pass" class="form-control validate">
-          <label data-error="wrong" data-success="right" for="defaultForm-pass">Your password</label>
-        </div>
-
-      </div>
-      <div class="modal-footer">
-        <button class="button">Login</button>
-      </div>
-    </div>
-  </div>
-</div>`;
+      </div  
+    `
 }
+
+
+// function modalBuilder(modal) {
+//     return `<div class="modal fade" id="modalContactForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+//   aria-hidden="true">
+//   <div class="modal-dialog" role="document">
+//     <div class="modal-content">
+//       <div class="modal-header text-center">
+//         <h4 class="modal-title w-100 font-weight-bold">Write to us</h4>
+//         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+//           <span aria-hidden="true">&times;</span>
+//         </button>
+//       </div>
+//       <div class="modal-body mx-3">
+//         <div class="md-form mb-5">
+//           <i class="fas fa-user prefix grey-text"></i>
+//           <input type="text" id="form34" class="form-control validate">
+//           <label data-error="wrong" data-success="right" for="form34">Your name</label>
+//         </div>
+//
+//         <div class="md-form mb-5">
+//           <i class="fas fa-envelope prefix grey-text"></i>
+//           <input type="email" id="form29" class="form-control validate">
+//           <label data-error="wrong" data-success="right" for="form29">Your email</label>
+//         </div>
+//
+//         <div class="md-form mb-5">
+//           <i class="fas fa-tag prefix grey-text"></i>
+//           <input type="text" id="form32" class="form-control validate">
+//           <label data-error="wrong" data-success="right" for="form32">Subject</label>
+//         </div>
+//
+//         <div class="md-form">
+//           <i class="fas fa-pencil prefix grey-text"></i>
+//           <textarea type="text" id="form8" class="md-textarea form-control" rows="4"></textarea>
+//           <label data-error="wrong" data-success="right" for="form8">Your message</label>
+//         </div>
+//
+//       </div>
+//       <div class="modal-footer d-flex justify-content-center">
+//         <button class="btn btn-unique">Send <i class="fas fa-paper-plane-o ml-1"></i></button>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+//
+// <div class="text-center">
+//   <a href="" class="btn btn-default btn-rounded mb-4" data-toggle="modal" data-target="#modalContactForm">Launch
+//     Modal Contact Form</a>
+// </div>`;
+// }
 
 
